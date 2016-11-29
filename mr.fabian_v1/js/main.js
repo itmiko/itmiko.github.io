d42c2cfefaa73bcf7cffa37013fd75235ea0e5f5
itmiko <mypost.mk@gmail.com>
2016-11-29 23:58:58 +0200

    added 2 variants of site

---
+++ mr.fabian_v1/js/main.js
@@ -0,0 +1,17 @@
+
+
+      function initMap() {
+
+        var uluru = {lat: 49.838278, lng: 24.032292};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: uluru,
+          scrollwheel:  false
+        });
+
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
---
+++ mr.fabian_v2/js/main.js
@@ -0,0 +1,28 @@
+//apperar image-logo
+window.onload=function(){
+    var image = document.getElementById("image-logo");
+    var i = 0,
+    display_arr = ["block","none"],
+    time_arr = [68000,5500]; //76s - block, 5.5s - none
+    function show()
+        {  i ^= 1;
+           image.style.display = display_arr[i];
+            setTimeout(show,time_arr[i]);
+        }
+    show();
+}
+
+// google maps
+function initMap() {
+    var uluru = {lat: 49.838278, lng: 24.032292};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: uluru,
+        scrollwheel:  false
+    });
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+};
