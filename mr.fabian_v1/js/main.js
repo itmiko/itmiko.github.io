dcce3e6c44470e2a4c04d02d45f07e9667482178
itmiko <mypost.mk@gmail.com>
2016-12-20 23:45:42 +0200

    added animation

--- mr.fabian_v1/js/main.js
+++ mr.fabian_v1/js/main.js
@@ -1,4 +1,9 @@
 
+jQuery(document).ready(function($) {
+  $(".master-item").animated("flipInY");
+  $("h2").animated("bounceInLeft");
+  $(".service-price").animated("zoomIn");
+});
       function initMap() {
 
         var uluru = {lat: 49.838278, lng: 24.032292};
